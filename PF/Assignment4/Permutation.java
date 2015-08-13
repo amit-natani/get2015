5f525e28b1118a022830df914adbda9981ab6d58
Amit Natani <amit.natani@metacube.com>
2015-08-13 14:59:35 +0100

    Assignment 4 updated

---
+++ PF/Assignment4/Permutation.java
@@ -0,0 +1,96 @@
+/**
+ *Program Title: to find permutations of a given string using Recursion
+ *Date: 12 Aug 2015
+ */
+import java.util.*;	// package imported to use Scanner Class
+
+/**
+ * @author Amit
+ *
+ */
+public class Permutation 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		/* initialization */
+		String inputString;			
+		Scanner sc = new Scanner(System.in);		
+		System.out.println("Enter a String: ");
+		inputString = sc.next();			
+		Permutation permutationObject=new Permutation();		
+		permutationObject.permute(inputString);		// permute method invocation with parameter inputString
+		sc.close();									// sc object closed
+	}
+	
+	/* this method takes input string as argument and call other method named doPermute with parameters 
+	 * char array inputArray.  List Object Result, boolean array characterUsed, length of input as inputLength
+	 * level and outputString as StringBuilder 
+	 */
+	List<String> permute(String inputString)
+	{
+	  int inputLength = inputString.length();	
+	  StringBuilder outputString = new StringBuilder();	
+	  boolean[ ] characterUsed = new boolean[ inputLength ];	// boolean array with size equal to inputLength
+	  List<String> Result = new ArrayList<String>();			
+	  char[ ] inputArray = inputString.toCharArray( );			// this will convert input string in character array
+	  
+	  
+	  /* the result of doPermute will be stored in List named result */
+	  Result = doPermute ( inputArray, Result, characterUsed, inputLength, 0, outputString);
+	  
+	  /* Printing all values stored in the List Result */
+	  for(String s : Result)
+	  {
+		  System.out.println(s);
+	  }
+	  return Result;		// Returning the list Result 
+
+	}
+	
+	/* recursive method with parameters 
+	 * char array inputArray.  List Object Result, boolean array characterUsed, length of input as inputLength
+	 * index and outputString as StringBuilder */
+	List<String> doPermute ( char[] inputArray, List<String> Result, boolean[ ] characterUsed, int inputLength, int index, StringBuilder outputString)
+	{
+		boolean flag = false;
+		
+		/* checking for the duplicate strings */
+		if( index == inputLength) 
+		{
+			for(String tempString : Result)
+			{
+				if(tempString.equals(new String(outputString)))
+				{
+					flag = true;
+					break;
+				}
+			}
+			if(flag == false)
+				/* this code will be executed if the permutation is not repeated */
+				Result.add(outputString.toString());		// Add permutations to List Result
+			return Result;			// returning the List Result
+		}
+		
+		for( int i = 0; i < inputLength; ++i )
+		{       
+	
+			if( characterUsed[i] ) 
+				continue;		// continue if character is already traced
+			
+			outputString.append( inputArray[i] );      // append the next character to outputArray
+			characterUsed[i] = true;       
+			doPermute( inputArray,   Result, characterUsed, inputLength, index + 1, outputString); // recursive calling with increased index
+			characterUsed[i] = false;       			
+			outputString.setLength(outputString.length() - 1 );	// decrementing length of outputString by 1 for next different character
+		}
+		return Result;  // returning the List Result
+	}
+}
+
+
+
+
---
+++ PF/Assignment4/TowerOfHanoiTest.java
@@ -0,0 +1,101 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import java.util.*;
+
+/**
+ * 
+ */
+
+/**
+ * @author Amit
+ *
+ */
+public class TowerOfHanoiTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+	/* test for 3 disks */
+	@Test
+	public void testWithThreeDisks() {	
+		TowerOfHanoi towerHanoiTestObj = new TowerOfHanoi();
+		List<String> actual = new ArrayList<String>();
+		String expected[]={"Move Disk   1   A->B", "Move Disk   2   A->C", "Move Disk   1   B->C", "Move Disk   3   A->B", "Move Disk   1   C->A", "Move Disk   2   C->B", "Move Disk   1   A->B"};
+		towerHanoiTestObj.towerOfHanoi("A", "B", "C", 3, actual);
+		int i=0;
+		for(String resultString : actual)
+		{
+			assertEquals("", expected[i], resultString);
+			i++;
+		}
+		//fail("Not yet implemented");
+	}
+	
+	
+	/* test for 1 disk */
+	@Test
+	public void testWithOneDisk() {
+		
+		TowerOfHanoi towerHanoiTestObj = new TowerOfHanoi();
+		List<String> actual = new ArrayList<String>();
+		String expected[]={"Move Disk   1   A->B"};
+		towerHanoiTestObj.towerOfHanoi("A", "B", "C", 1, actual);
+		int i=0;
+		for(String resultString:actual)
+		{
+			assertEquals("", expected[i], resultString);
+			i++;
+		}
+		//fail("Not yet implemented");
+	}
+	
+	
+	/* test for 2 disk */
+	@Test
+	public void testWithTwoDisks() {
+		
+		TowerOfHanoi towerHanoiTestObj = new TowerOfHanoi();
+		List<String> actual = new ArrayList<String>();
+		String expected[]={"Move Disk   1   A->C", "Move Disk   2   A->B", "Move Disk   1   C->B"};
+		towerHanoiTestObj.towerOfHanoi("A", "B", "C", 2, actual);
+		int i=0;
+		for(String resultString : actual)
+		{
+			assertEquals("", expected[i], resultString);
+			i++;
+		}
+		//fail("Not yet implemented");
+	}
+
+}
