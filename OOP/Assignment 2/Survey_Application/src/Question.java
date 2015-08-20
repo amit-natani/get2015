5ad36a6721322efdd4cdd0ac40b58935c3d8f67f
amit-natani <amit.natani@metacube.com>
2015-08-20 10:47:32 +0100

    Update Question.java

--- OOP/Assignment 2/Survey_Application/src/Question.java
+++ OOP/Assignment 2/Survey_Application/src/Question.java
@@ -11,7 +11,7 @@ public interface Question {
 	public String age(boolean flag);
 	public String nationality(boolean flag);
 	public String gender(boolean flag);
-	public String ratingHaritages(boolean flag);
-	public String favouriteHaritages(boolean flag);
+	public String ratingHeritages(boolean flag);
+	public String favoriteHeritages(boolean flag);
 	public String feedback(boolean flag);
 }
