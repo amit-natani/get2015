f8e80f4f6e4fb19f871459d7f621885f9a68f0f5
amit-natani <amit.natani@metacube.com>
2015-08-24 14:39:17 +0100

    Update Constants.java

--- OOP/Assignment 5/Social_Networking_Site/src/Constants.java
+++ OOP/Assignment 5/Social_Networking_Site/src/Constants.java
@@ -1,7 +1,7 @@
 
 public class Constants {
 	
-	final static String PERSONFILE = "E:\\Person1.txt";
+	final static String PERSONFILE = "E:\\Person.txt";
 	final static String ORGANISATIONFILE = "E:\\Organisation.txt";
 
 }
