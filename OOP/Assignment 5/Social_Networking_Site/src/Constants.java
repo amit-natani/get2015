2b02557623e09639c37be3ff556daa08d3c7651e
Amit Natani <amit.natani@metacube.com>
2015-08-26 08:44:12 +0100

    Assignmet 5 Social Network Updated

--- OOP/Assignment 5/Social_Networking_Site/src/Constants.java
+++
@@ -1,7 +0,0 @@
-
-public class Constants {
-	
-	final static String PERSONFILE = "E:\\Person1.txt";
-	final static String ORGANISATIONFILE = "E:\\Organisation.txt";
-
-}
--- OOP/Assignment 5/Social_Networking_Site/src/Record.java
+++
@@ -1,5 +0,0 @@
-
-public interface Record {
-
-	public void getRecords();
-}
