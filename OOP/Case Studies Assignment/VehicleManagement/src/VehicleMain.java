1895a2110c57da496b91c452b9434bc2b5f0086a
amit-natani <amit.natani@metacube.com>
2015-09-15 09:51:56 +0100

    Update VehicleMain.java

--- OOP/Case Studies Assignment/VehicleManagement/src/VehicleMain.java
+++ OOP/Case Studies Assignment/VehicleManagement/src/VehicleMain.java
@@ -9,13 +9,16 @@ public class VehicleMain {
 	public static void main(String[] args) {
 		
 		/* Car object creation */
-		Car carObj = VehicleHelper.create("Hyundai", "Verna", "1200 CC", "60 Liters", "21 km/pl", 2, 5);
+		Car carObj = VehicleHelper.create("Hyundai", "Verna", "1200 CC", "60 Liters", "21 km/pl", 900000, 78000, "YES", "YES", "YES");
 		System.out.println( VehicleDetailPrinter.printVehicleSpecification(carObj));
+		System.out.println( "On road Price : "+VehicleDetailPrinter.printOnRoadPrice(carObj)+"\n");
 		
 		/* Bike object creation */
-		Bike bikeObj = VehicleHelper.create("TVS", "Apache", "180 CC", "14 Liters", "40 km/pl", 5, "Stepped");
+		Bike bikeObj = VehicleHelper.create("TVS", "Apache", "180 CC", "14 Liters", "40 km/pl", 80000, 7800, "YES", 1000);
 		System.out.println( VehicleDetailPrinter.printVehicleSpecification(bikeObj));
+		System.out.println( "On road Price : "+VehicleDetailPrinter.printOnRoadPrice(bikeObj)+"\n");
 
 	}
 
 }
+
