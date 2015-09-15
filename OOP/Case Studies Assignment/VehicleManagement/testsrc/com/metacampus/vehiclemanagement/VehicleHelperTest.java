c88829b9636150e5e7c33e871c4872165ea18c33
amit-natani <amit.natani@metacube.com>
2015-09-15 09:48:52 +0100

    Update VehicleHelperTest.java

--- OOP/Case Studies Assignment/VehicleManagement/testsrc/com/metacampus/vehiclemanagement/VehicleHelperTest.java
+++ OOP/Case Studies Assignment/VehicleManagement/testsrc/com/metacampus/vehiclemanagement/VehicleHelperTest.java
@@ -17,27 +17,28 @@ public class VehicleHelperTest {
 
 	@Test
 	public void testCreateCar() {
-		Car car = VehicleHelper.create("Hyundai", "Verna", "1200 CC", "60 Liters", "21 km/pl", 2, 5);
-		assertEquals("Hyundai", car.getMake());
+		Car carObj = VehicleHelper.create("Hyundai", "Verna", "1200 CC", "60 Liters", "21 km/pl", 900000, 78000, "YES", "YES", "YES");
+		assertEquals("Hyundai", carObj.getMake());
 	}
 
 	@Test 
 	public void testCreateCarNullTest() {
-		Car car = VehicleHelper.create("Hyundai", "Verna", "1200 CC", "60 Liters", "21 km/pl", 2, 5);
-		assertNotNull(car);
+		Car carObj = VehicleHelper.create("Hyundai", "Verna", "1200 CC", "60 Liters", "21 km/pl", 900000, 78000, "YES", "YES", "YES");
+		assertNotNull(carObj);
 	}
 	
 	@Test
 	public void testCreateBike() {
-		Bike bike = VehicleHelper.create("TVS", "Apache", "180 CC", "14 Liters", "40 km/pl", 5, "Stepped");
-		assertEquals("180 CC",  bike.getEngineCC());
+		Bike bikeObj = VehicleHelper.create("TVS", "Apache", "180 CC", "14 Liters", "40 km/pl", 80000, 7800, "YES", 1000);
+		assertEquals("180 CC",  bikeObj.getEngineCC());
 	}
 	
 	@Test
 	public void testCreateBikeNullTest() {
-		Bike bike = VehicleHelper.create("TVS", "Apache", "180 CC", "14 Liters", "40 km/pl", 5, "Stepped");
-		assertNotNull(bike);
+		Bike bikeObj = VehicleHelper.create("TVS", "Apache", "180 CC", "14 Liters", "40 km/pl", 80000, 7800, "YES", 1000);
+		assertNotNull(bikeObj);
 	}
 
 }
+
 
