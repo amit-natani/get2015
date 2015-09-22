0edb934ee6d2bf329e4dce553d5d79bfc7872d7c
Amit Natani <amit.natani@metacube.com>
2015-09-22 14:06:46 +0100

    Case Study Phase 2

---
+++ DBMS/Case Study Phase 2/VehicleManagement/xml/com/metacampus/vehiclemanagement/xml/ReadConnectionMain.java
@@ -0,0 +1,11 @@
+package com.metacampus.vehiclemanagement.xml;
+
+public class ReadConnectionMain {
+   
+	public static void main(String args[]) {
+	     
+		/* Reading Connection XML File */
+		ReadConnectionXml read = new ReadConnectionXml();
+	    read.readConfig("connection.xml");
+	 }
+}
