8a06e445e21f8c9fe13245514b4745ae7225e89e
amit-natani <amit.natani@metacube.com>
2015-10-21 17:10:11 +0530

    Update LoginValidation.java

--- Web/JSPSession1/src/com/servlets/LoginValidation.java
+++ Web/JSPSession1/src/com/servlets/LoginValidation.java
@@ -7,7 +7,6 @@ import java.sql.Statement;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,46 +31,69 @@ public class LoginValidation extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		String username, password;
-		username = request.getParameter("username");
-		password = request.getParameter("password");
-		String query = "select * from registration where username='"+username+"'";
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		conn = ConnectionUtil.getConnection();
-		RequestDispatcher reqDisSuccess = request.getRequestDispatcher("loginsuccess.jsp");
-		RequestDispatcher reqDisFailure = request.getRequestDispatcher("login.jsp");
-		try {
-			stmt = conn.createStatement();
-			rs = stmt.executeQuery(query);
-			if(rs.next()) {
-				if(rs.getString("password").equals(password)) {
-					request.setAttribute("email", rs.getString("email"));
-					reqDisSuccess.forward(request, response);
-				}
-				else {
-					request.setAttribute("a", "Incorrect Password");
-					reqDisFailure.forward(request, response);
-				}
-			}
-			else {
-				request.setAttribute("a", "Incorrect username");
-				reqDisFailure.forward(request, response);
-			}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		boolean flag = loginValidation(request, response);
+		if(flag==true)
+		{
+			RequestDispatcher reqDisSuccess = request.getRequestDispatcher("loginsuccess.jsp");
+			reqDisSuccess.forward(request, response);
 		}
-		catch(Exception e) {
-			response.sendRedirect("errorpage.jsp");
+		else
+		{
+			RequestDispatcher reqDisFailure = request.getRequestDispatcher("login.jsp");
+			reqDisFailure.forward(request, response);
 		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		doPost(request, response);
 	}
-
+	
+	private boolean loginValidation(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		boolean flag = false;
+		response.setContentType("text/html");
+		String username, password;
+		username = request.getParameter("username");
+		password = request.getParameter("password");
+		String query = "SELECT username, password, email FROM registration WHERE username='"+username+"'";
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		conn = ConnectionUtil.getConnection();
+		
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
+			if(rs.next()) 
+			{
+				if(rs.getString("password").equals(password)) 
+				{
+					request.setAttribute("email", rs.getString("email"));
+					flag = true;
+				}
+				else 
+				{
+					request.setAttribute("a", "Incorrect Password");			
+				}
+			}
+			
+			else 
+			{
+				request.setAttribute("a", "Incorrect username");	
+			}
+			
+		}
+		catch(Exception e) 
+		{
+			RequestDispatcher requestDispatcher;
+			requestDispatcher = request.getRequestDispatcher("errorpage.jsp");
+			requestDispatcher.forward(request, response);
+		}
+		return flag;
+	}
 }
